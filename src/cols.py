507fb120c2a8719e7e25434c8b30aab786a19480
ATHARVA47 <atharvakale1234@gmail.com>
2024-01-15 18:52:07 -0500

    Created cols.py

---
+++ src/cols.py
@@ -0,0 +1,30 @@
+import re
+
+class Cols:
+    def __init__ (self, row):
+        self.x=[]
+        self.y=[]
+        self.all = []
+        self.klass=None
+        self.names= row
+
+        for name_col in row:
+            n= row.index(name_col)
+            name_col = name_col.strip()
+            if name_col[0].islower():
+                col = SYM(name_col,n)
+            else:
+                col = NUM(name_col, n)
+            self.all.append(col)
+            if not name_col[-1] == "X":
+                if "!" in name_col:
+                    self.klass =col
+                if re.findall("[!+-]$", name_col):
+                    self.y.append(col)
+                else:
+                    self.x.append(col)
+
+def add(self, row):
+        for A in [self.x, self.y]:
+            for col in A:
+                col.add(row.cells[col.at])
