d86560ced4b0db9ce0033bdee0df32a6aa73812e
SnigdhaRao02 <kotharusnigdha2002@gmail.com>
2024-01-15 18:08:08 -0500

    adding sym class

---
+++ src/sym.py
@@ -0,0 +1,36 @@
+import math
+class Sym:
+    def __init__(self,s="",n=0) -> None:
+        self.at= n
+        self.txt= s
+        self.n=0    
+        self.mode = "" #mean
+        self.most = 0
+        self.has={}
+       
+        #heaven to be added
+    def add(self,x):
+        if x!="?":
+            self.n+=1
+            if(x not in self.has.keys()):
+                self.has[x]=0
+            else:
+                self.has[x]+=1
+            if(self.has[x]>self.most):
+                self.most,self.mode = self.has[x], x
+
+    def mid(self):
+        return self.mode
+    def div(self,e):
+        e=0
+        for k in self.has:
+            e=e-self.has[k]/self.n*math.log(self.has[k]/self.n,2)
+
+        return e
+
+        
+    
+        
+
+      
+
