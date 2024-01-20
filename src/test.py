ff4f6725e854876d75379c1ce51bbbdf9d9944bf
skothar2 <skothar2@ncsu.edu>
2024-01-19 19:05:46 -0500

    changes to test.py

--- src/test.py
+++ src/test.py
@@ -12,7 +12,7 @@ def eg_num():
     mu,sd=e.mid(),e.div()
     print(round(mu,3),round(sd,3))
     return(mu>10 and mu<10.1 and sd>2 and sd<2.05)
-print(eg_num())
+
 
 def eg_sym():
     s=Sym()
@@ -21,11 +21,11 @@ def eg_sym():
     mode,e=s.mid(),s.div()
     print(mode,e)
     return(e>1.37 and e<1.38 and mode==1)
-print(eg_sym())
+
 
 def eg_data():
     n=0
-    d=Data("/Users/challasaicharitha/gate/data/auto93.csv")
+    d=Data("/Users/Neelr/Desktop/ASE-24-Grp20/data/auto93.csv")
     i=1
     for row in d.rows:
         print(row)
@@ -34,7 +34,34 @@ def eg_data():
       #  i+=1
         n+=1
     return(n==399)
-print(eg_data())
+
+
+def run_all():
+    num_res=eg_num()
+    sym_res=eg_sym()
+    data_res=eg_data()
+
+    print("Num Class:",num_res)
+    print("Sym Class:",sym_res)
+    print("Data Class:",data_res)
+
+#run_all()
+
+def run_tests(test_name):
+    if(test_name=='all'):
+        run_all()
+    elif(test_name=='num'):
+        print(eg_num())
+    elif(test_name=='sym'):
+        print(eg_sym())
+    elif(test_name=='data'):
+        print(eg_data())
+
+#run_tests('all')
+
+    
+
+
         
 
 
